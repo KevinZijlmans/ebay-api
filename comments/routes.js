f8bede70c0964353a8fc5887d84f3157c4846117
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-07-10 12:43:34 +0200

    fixed comment create

--- comments/routes.js
+++ comments/routes.js
@@ -3,7 +3,7 @@ const Comment = require('./model')
 
 const router = new Router()
 
-  router.post('/adverts/:id/comments', (req, res, next) => {
+  router.post('/comments', (req, res, next) => {
     const text = req.body.text
     const advertId = req.params.id
 
